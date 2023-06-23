36ef90d513ffe6628a60c71b92c4739e54945eef
AddisonFP <addrad@icloud.com>
2023-06-23 09:18:10 -0600

    test 2 not working, test 1 needs analytical answer

--- Testing/B1_test.m
+++ Testing/B1_test.m
@@ -1,12 +1,18 @@
+%% Unit test that compares B1 and B1map to analytical solutions
+% test #1: test that the flip angle matches the area underneath the B1
+% curve
+% test #2: test that a spatially varying non uniform B1 map functions as expected
 classdef B1_test < matlab.unittest.TestCase
     properties
     end
 
     methods (Test)
-        function final_state_err(testCase)
-            clear, close all
-            addpath(genpath('./'))
-
+        %tests different flip angles 1-90
+        function diff_FA(testCase)
+            addpath(genpath('./'));
+            
+            Gz = 30;
+            dz = 5;
             T = 2; % s
             z = -5:5;
             sz = size(z);
@@ -15,12 +21,16 @@ classdef B1_test < matlab.unittest.TestCase
                     zeros(size(z))
                     z], [1, 3, 2]);
             grad = zeros(3, ceil(T/dt));
-            B1 = zeros(1, ceil(T/dt));
-            T1 = 100:100:1100*ones(sz);
-            T2 = 50*ones(sz);
-            
-            [result, ~] = bloch_symmetric_splitting(dt, B1, grad, pos, T1, T2, Minit = [0; 1; 0]); %computed solution
-            expected = permute([zeros(1, 11); exp(-T*1./T2); ones(1, 11)-exp(-T./T1)], [1, 3, 2]); %analytical solution
+            T1 = inf*ones(sz);
+            T2 = inf*ones(sz);
+
+            for FA=1:90
+                [B1e, ~] = b1_sliceselect(Gz, dz, FA, T, dt);
+                
+                [result, ~] = bloch_symmetric_splitting(dt, B1e, grad, pos, T1, T2); %computed solution
+                expected = permute([zeros(1, 11); exp(-T*1./T2); ones(1, 11)-exp(-T./T1)], [1, 3, 2]); %analytical solution
+                testCase.verifyLessThan(abs(result(3, 1, :) - expected(3, 1, :)), 1e-10);
+            end
             
             figure()
             plot(T1, permute(result(3, 1, :), [1, 3, 2]))
@@ -30,7 +40,33 @@ classdef B1_test < matlab.unittest.TestCase
             grid on
             xlabel('T1 time (ms)')
             ylabel('horizontal magnitude')  
-            testCase.verifyEqual(result(3, 1, :), expected(3, 1, :))
+            
+        end
+        
+        % non uniform B1map test case
+        function non_uniform_B1map(testCase)
+            addpath(genpath('./'));
+            
+            Gz = 30;
+            dz = 5;
+            T = 2; % s
+            z = -5:5;
+            sz = size(z);
+            dt = 1e-3; % ms
+            pos = permute([zeros(size(z))
+                    zeros(size(z))
+                    z], [1, 3, 2]);
+            grad = zeros(3, ceil(T/dt));
+            T1 = inf*ones(sz);
+            T2 = inf*ones(sz);
+            [B1e, ~] = b1_sliceselect(Gz, dz, 90, T, dt);
+            B1map = .9001:.0001:1.1;
+            
+            [result, ~] = bloch_symmetric_splitting(dt, B1e, grad, pos, T1, T2, B1map=B1map); %computed solution
+            expected = permute([zeros(1, 11); exp(-T*1./T2); ones(1, 11)-exp(-T./T1)], [1, 3, 2]); %analytical solution
+            testCase.verifyLessThan(abs(result(3, 1, :) - expected(3, 1, :)), 1e-10);
+            
+            
         end
     end
 end
